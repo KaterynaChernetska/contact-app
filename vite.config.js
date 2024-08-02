17cc73ccd234e6938b816cd8e476edeace945ba4
Kateyna <vesnavved@gmail.com>
2024-08-02 18:45:24 +0300

    add vercel setup

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/contact-app",
   plugins: [react()],
-
+  // base: '/contact-app/',
   server: {
     proxy: {
-      "/api": {
-        target: "https://live.devnimble.com",
+      '/api': {
+        target: 'https://live.devnimble.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "/api/v1"),
-      },
-    },
-  },
+        secure: false
+      }
+    }
+  }
 });
